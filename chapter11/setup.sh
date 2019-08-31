b74a2a58e84ccc120474b44194195c9e0b63b410
Peng Xiao <xiaoquwl@gmail.com>
2019-08-31 13:40:10 +0200

    rename
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- chapter11/setup.sh
+++ chapter11/setup.sh
@@ -2,3 +2,14 @@
 
 # install some tools
 sudo yum install -y git vim gcc glibc-static telnet
+
+# install docker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# start docker service
+sudo groupadd docker
+sudo gpasswd -a vagrant docker
+sudo systemctl start docker
+
+rm -rf get-docker.sh
